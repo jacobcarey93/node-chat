0bee27504f55dbbf39a25c458fef96f0c499db3e
jacobcarey93 <31251057+jacobcarey93@users.noreply.github.com>
2017-09-13 15:57:55 -0600

    minor changes

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,23 +1,23 @@
-let messages = [];
-let id = 0;
+var messages = [];
+var id = 0;
 
 module.exports = {
-  create: ( req, res ) => {
-    const { text, time } = req.body;
+  create: ( req, res, next ) => {
+    var  {text, time} = req.body;
     messages.push({ id, text, time });
     id++;
     res.status(200).send( messages );
   },
 
-  read: ( req, res ) => {
+  read: ( req, res, next ) => {
     res.status(200).send( messages );
   },
 
-  update: ( req, res ) => {
-    const { text } = req.body;
-    const updateID = req.params.id;
-    const messageIndex = messages.findIndex( message => message.id == updateID );
-    let message = messages[ messageIndex ];
+  update: ( req, res, next ) => {
+    var {text}  = req.body;
+    var updateID = req.params.id;
+    var messageIndex = messages.findIndex( message => message.id == updateID );
+    var message = messages[ messageIndex ];
 
     messages[ messageIndex ] = {
       id: message.id,
@@ -28,8 +28,8 @@ module.exports = {
     res.status(200).send( messages );
   },
 
-  delete: ( req, res ) => {
-    const deleteID = req.params.id;    
+  delete: ( req, res, next ) => {
+    var deleteID = req.params.id;    
     messageIndex = messages.findIndex( message => message.id == deleteID );
     messages.splice(messageIndex, 1);
     res.status(200).send( messages );
